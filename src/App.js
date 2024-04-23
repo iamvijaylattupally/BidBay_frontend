5cc83d5bc719bb251ed5b3901f4139699d40aec4
sicmundus8309 <reddyvijay1690@gmail.com>
2024-04-23 23:38:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoutes from "./ProtectedRoutes";
+import Auth from './components/Auth/Auth';
+import Home from './pages/Home/Home';
+import Cart from "./pages/Cart/Cart";
+import SellingPage from './pages/SellingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+
+        <Routes>
+          <Route path="/signup" element={<Auth />} />
+
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRoutes>
+                  <Cart />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/sellerpage"
+            element={
+              <ProtectedRoutes>
+                  <SellingPage />
+              </ProtectedRoutes>
+            }
+          />
+
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
